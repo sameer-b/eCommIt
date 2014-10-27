6dab9c4a233e5edd297cf238dc768ee9d13c5889
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2014-10-27 19:37:24 -0400

    added remember me functionality and css bootstrap

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var um = require('./lib/userManagement.js');
 var cookieParser = require('cookie-parser');
+var path = require('path');
 var app = express();
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+
 
 app.get('/register', function (request, response) {
-  response.end("<html><head><title></title></head><body><form action=\"/addUser\" method=\"POST\"> Name: <input name=\"name\"/> Email: <input name=\"email\"/> Password: <input name=\"password\"/> Confirm Password: <input name=\"confirmPassword\"/><button type=\"submit\">Register</button></form></body></html>");
+	response.render('register')
+	response.end();
+ //response.end("<html><head><title></title></head><body><form action=\"/addUser\" method=\"POST\"> Name: <input name=\"name\"/> Email: <input name=\"email\"/> Password: <input name=\"password\"/> Confirm Password: <input name=\"confirmPassword\"/><button type=\"submit\">Register</button></form></body></html>");
 });
 
 
@@ -15,7 +24,9 @@ app.post('/addUser', function (request, response) {
 });
 
 app.get('/login', function (request, response) {
-  response.end("<html><head><title></title></head><body><form action=\"/authUser\" method=\"POST\">  Email: <input name=\"email\"/> Password: <input name=\"password\"/> <button type=\"submit\">Register</button></form></body></html>");
+  response.render('login');
+  //response.end();
+  //response.end("<html><head><title></title></head><body><form action=\"/authUser\" method=\"POST\">  Email: <input name=\"email\"/> Password: <input name=\"password\"/> <button type=\"submit\">Register</button></form></body></html>");
 
 });
 
@@ -26,6 +37,7 @@ app.post('/authUser', function (request, response) {
 app.get('/home', function (request, response) {
     var content = "<html><head></head><body><h1>Home</h1></body></html>";
     um.authenticateUser(request.cookies, response, content);
+   // response.render('home');
 });
 
 
